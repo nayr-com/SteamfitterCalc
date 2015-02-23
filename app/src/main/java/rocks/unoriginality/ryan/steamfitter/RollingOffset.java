4f576f6b21a46efe5cfb10f557a8e4a114c01df1
Ryan <ryanmavilia@gmail.com>
2015-02-23 00:15:37 -0500

    Fixed math errors and changing variable names, also added Javadoc comments.

--- app/src/main/java/rocks/unoriginality/ryan/steamfitter/RollingOffset.java
+++ app/src/main/java/rocks/unoriginality/ryan/steamfitter/RollingOffset.java
@@ -14,13 +14,13 @@ import static java.lang.Double.parseDouble;
 
 public class RollingOffset extends ActionBarActivity implements View.OnClickListener {
     Button calculate;
-    TextView A_Text,travelText,runText;
+    TextView trueOffsetText,travelText,runText;
     EditText rollInput,setInput,angleInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equation_one);
-        A_Text=(TextView) findViewById(R.id.A_E1);
+        trueOffsetText =(TextView) findViewById(R.id.trueOffsetE1);
         travelText=(TextView) findViewById(R.id.travelE1);
         runText=(TextView) findViewById(R.id.runE1);
         rollInput=(EditText) findViewById(R.id.rollTextInput);
@@ -29,7 +29,6 @@ public class RollingOffset extends ActionBarActivity implements View.OnClickList
         calculate=(Button) findViewById(R.id.calculateButtonE1);
         calculate.setOnClickListener(this);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,17 +51,47 @@ public class RollingOffset extends ActionBarActivity implements View.OnClickList
 
         return super.onOptionsItemSelected(item);
     }
-
+    /**
+     * When the "Calculate" button is pressed
+     * this method will check to see which numbers
+     * the user typed in for the Rolling Offset equation
+     * set. There are three different outputs that could
+     * be genrated.
+     *
+     * Note this method uses the isEmpty method:
+     * @see RollingOffset#isEmpty(android.widget.EditText)
+     *
+     * 1. If the roll or set is not provided then
+     * no calculation will be complete and errors
+     * will be shown to the user to indicate this.
+     * If the user provides an angle without one or
+     * both parameters errors will still be thrown
+     * as those parameters are necessarry for all
+     * calculations.
+     *
+     * 2. If roll and set are provided and angle
+     * is not then the travel and run will not be
+     * calculated and errors will be thrown for at
+     * the output locations for these variables.
+     * The true offset will still be calculated and
+     * shown to the user.
+     *
+     * 3. If all three parameters are provided then
+     * the run, travel, and true offset will be calculated
+     * and given to the user.
+     *
+     * @param v specifies the view that was clicked.
+     */
     public void onClick(View v) {
         if(isEmpty(rollInput) || isEmpty(setInput)) {
-            A_Text.setText("Invalid roll or set");
+            trueOffsetText.setText("Invalid roll or set");
             travelText.setText("Invalid angle");
             runText.setText("Invalid angle");
         } else if(isEmpty(angleInput)){
             double roll=parseDouble(rollInput.getText().toString());
             double set=parseDouble(setInput.getText().toString());
             double A = Math.sqrt((roll * roll) + (set * set));
-            A_Text.setText("A: "+Double.toString(A));
+            trueOffsetText.setText("True offset: " + Double.toString(A));
             travelText.setText("Invalid angle");
             runText.setText("Invalid angle");
         } else {
@@ -70,14 +99,22 @@ public class RollingOffset extends ActionBarActivity implements View.OnClickList
             double set=parseDouble(setInput.getText().toString());
             double angle=parseDouble(angleInput.getText().toString());
             double A = Math.sqrt((roll * roll) + (set * set));
-            double travel=A*(1.0/Math.sin(angle));
-            double run=A*(1.0/Math.tan(angle));
-            A_Text.setText("A: "+Double.toString(A));
+            double travel=A*(1.0/Math.sin(Math.toRadians(angle)));
+            double run=A*(1.0/Math.tan(Math.toRadians(angle)));
+
+            trueOffsetText.setText("A: " + Double.toString(A));
             travelText.setText("Travel: "+Double.toString(travel));
             runText.setText("Run: "+Double.toString(run));
         }
     }
-    private boolean isEmpty(EditText myeditText) {
-        return myeditText.getText().toString().trim().length() == 0;
+
+    /**
+     * Checks to see if
+     * @param myEditText the text input you want to check
+     * @return a true/false that tells if
+     * an input is empty
+     */
+    private boolean isEmpty(EditText myEditText) {
+        return myEditText.getText().toString().trim().length() == 0;
     }
 }
